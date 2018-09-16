6957f32e6b6a88fa432532fc45c9fe18af0cc8a0
Dibble <james.cotterell@newvoicemedia.com>
2018-09-16 10:21:47 +0100

    Switch to Python

---
+++ sudoku.py
@@ -0,0 +1,22 @@
+class Sudoku:
+    puzzle = newPuzzle()
+
+    def setValue(self, x, y, value):
+        if value < 0 or value > 9:
+            return False
+
+        self.puzzle[x][y] = value
+
+
+def newPuzzle():
+    return [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
