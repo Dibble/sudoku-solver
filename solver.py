e851e7bac980ee34a1b53e8b9e26b58496f298f4
Dibble <james.cotterell@newvoicemedia.com>
2018-10-03 22:29:34 +0100

    First attempt at identifying unique options (not working). Fill single empty cell in a row. Fixed broken references to column and row values.

--- solver.py
+++ solver.py
@@ -12,7 +12,8 @@ class Solver:
 
         while isSolved != True and i < 1000:
             self.updateOptions()
-            print(f"iteration {i}")
+            # self.findUniqueOptions()
+            self.fillSingles()
 
             i += 1
             isSolved = self.isSolved()
@@ -28,25 +29,73 @@ class Solver:
         return True
 
     def updateOptions(self):
-        for i in range(9):
-            for j in range(9):
-                if self.puzzle[i][j]['value'] != None:
+        for column in range(9):
+            for row in range(9):
+                if self.puzzle[column][row]['value'] != None:
                     continue
 
-                rowValues = self.__getRowValues__(j)
-                columnValues = self.__getColumnValues__(i)
-                squareValues = self.__getSquareValues__(i, j)
+                rowValues = self.__getRowValues__(row)
+                columnValues = self.__getColumnValues__(column)
+                squareValues = self.__getSquareValues__(column, row)
 
                 rowAndColumnValues = rowValues + \
                     list(set(columnValues) - set(rowValues))
                 allValues = rowAndColumnValues + \
                     list(set(squareValues) - set(rowAndColumnValues))
 
-                self.__removeOptions__(i, j, allValues)
+                self.__removeOptions__(column, row, allValues)
 
-                if len(self.puzzle[i][j]['options']) == 1:
-                    self.puzzle[i][j]['value'] = self.puzzle[i][j]['options'][0]
-                    print(f"Solved [{i}][{j}]: {self.puzzle[i][j]['value']}")
+                if len(self.puzzle[column][row]['options']) == 1:
+                    self.puzzle[column][row]['value'] = self.puzzle[column][row]['options'][0]
+                    print(
+                        f"Update Options solved [{column}][{row}]: {self.puzzle[column][row]['value']}")
+
+    def findUniqueOptions(self):
+        for column in range(9):
+            for row in range(9):
+                if self.puzzle[column][row]['value'] != None:
+                    continue
+
+                rowOptions = self.__getRowOptions__(row)
+                for rowOption in range(9):
+                    if rowOptions[rowOption] == 1:
+                        self.puzzle[column][row]['value'] = rowOption + 1
+                        print(
+                            f"Find Unique Options solved [{column}][{row}]: {self.puzzle[column][row]['value']}")
+                        break
+
+                columnOptions = self.__getColumnOptions__(column)
+                for columnOption in range(9):
+                    if columnOptions[columnOption] == 1:
+                        self.puzzle[column][row]['value'] = columnOption + 1
+                        print(
+                            f"Find Unique Options solved [{column}][{row}]: {self.puzzle[column][row]['value']}")
+                        break
+
+                squareOptions = self.__getSquareOptions__(column, row)
+                for squareOption in range(9):
+                    if squareOptions[squareOption] == 1:
+                        self.puzzle[column][row]['value'] = squareOption + 1
+                        print(
+                            f"Find Unique Options solved [{column}][{row}]: {self.puzzle[column][row]['value']}")
+                        break
+
+    def fillSingles(self):
+        for row in range(9):
+            rowSum = 0
+            emptyCells = 0
+            emptyColumn = None
+            for col in range(9):
+                if self.puzzle[col][row]['value'] != None:
+                    rowSum += self.puzzle[col][row]['value']
+                else:
+                    emptyCells += 1
+                    emptyColumn = col
+
+            if emptyCells == 1:
+                self.puzzle[emptyColumn][row]['value'] = 45 - rowSum
+                print(
+                    f"Fill Singles solved [{emptyColumn}][{row}]: {self.puzzle[emptyColumn][row]['value']}")
 
     def __getRowValues__(self, row):
         rowValues = []
@@ -62,13 +111,6 @@ class Solver:
                 columnValues.append(self.puzzle[col][row]['value'])
         return columnValues
 
-    def __removeOptions__(self, col, row, options):
-        for val in options:
-            try:
-                self.puzzle[col][row]['options'].remove(val)
-            except ValueError:
-                continue
-
     def __getSquareValues__(self, col, row):
         squareValues = []
         squareX = math.floor(col / 3)
@@ -83,3 +125,39 @@ class Solver:
                     squareValues.append(self.puzzle[x][y]['value'])
 
         return squareValues
+
+    def __getRowOptions__(self, row):
+        rowOptions = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for col in range(9):
+            for option in range(len(self.puzzle[col][row]['options'])):
+                rowOptions[option - 1] += 1
+        return rowOptions
+
+    def __getColumnOptions__(self, col):
+        columnOptions = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for row in range(9):
+            for option in range(len(self.puzzle[col][row]['options'])):
+                columnOptions[option - 1] += 1
+        return columnOptions
+
+    def __getSquareOptions__(self, col, row):
+        squareOptions = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        squareX = math.floor(col / 3)
+        squareY = math.floor(row / 3)
+
+        for i in range(3):
+            for j in range(3):
+                x = squareX * 3 + i
+                y = squareY * 3 + j
+
+                for option in range(len(self.puzzle[x][y]['options'])):
+                    squareOptions[option - 1] += 1
+
+        return squareOptions
+
+    def __removeOptions__(self, col, row, options):
+        for val in options:
+            try:
+                self.puzzle[col][row]['options'].remove(val)
+            except ValueError:
+                continue
--- sudoku.py
+++ sudoku.py
@@ -3,67 +3,81 @@ def newPuzzle():
 
   for i in range(9):
     puzzle.append([])
-    for j in range(9):
+    for _ in range(9):
       puzzle[i].append({"value": None, "options": [1,2,3,4,5,6,7,8,9]})
   
   return puzzle
 
 def easyPuzzle():
   return [
-        [{"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 5, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 5, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 7, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}],
-        [{"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 0, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 5, "options": [1,2,3,4,5,6,7,8,9]}]
+        [{"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 5, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 5, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 7, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 5, "options": [1,2,3,4,5,6,7,8,9]}]
+    ]
+
+def mediumPuzzle():
+    return [
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 7, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": 5, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 3, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 1, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 6, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 7, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 4, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 9, "options": [1,2,3,4,5,6,7,8,9]}],
+        [{"value": 9, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 8, "options": [1,2,3,4,5,6,7,8,9]}, {"value": 2, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}, {"value": None, "options": [1,2,3,4,5,6,7,8,9]}]
     ]
 
 
 class Sudoku:
     def __init__(self):
       # self.puzzle = newPuzzle()
-      self.puzzle = easyPuzzle()
-      self.posX = 0
-      self.posY = 0
+      # self.puzzle = easyPuzzle()
+      self.puzzle = mediumPuzzle()
+      self.selectedColumn = 0
+      self.selectedRow = 0
 
     def setValue(self, value):
         if value < 0 or value > 9:
             return False
 
-        self.puzzle[self.posX][self.posY]['value'] = value
+        self.puzzle[self.selectedColumn][self.selectedRow]['value'] = value
         return value
 
     def move(self, direction):
         if direction == "UP":
-            if self.posX > 0:
-                self.posX -= 1
+            if self.selectedRow > 0:
+                self.selectedRow -= 1
         elif direction == "RIGHT":
-            if self.posY < 8:
-                self.posY += 1
+            if self.selectedColumn < 8:
+                self.selectedColumn += 1
         elif direction == "DOWN":
-            if self.posX < 8:
-                self.posX += 1
+            if self.selectedRow < 8:
+                self.selectedRow += 1
         elif direction == "LEFT":
-            if self.posY > 0:
-                self.posY -= 1
+            if self.selectedColumn > 0:
+                self.selectedColumn -= 1
 
     def print(self, printOptions):
         output = "====================================================================\n" if printOptions else "=========================================\n"
 
-        for i in range(len(self.puzzle)):
-            for j in range(len(self.puzzle[i])):
-                output += "||" if j % 3 == 0 else "|"
-                output += "=" if i == self.posX and j == self.posY else " "
-                output += f"{self.puzzle[i][j]['value']}" if self.puzzle[i][j]['value'] != None else " "
-                output += f"({len(self.puzzle[i][j]['options'])})" if printOptions else ""
-                output += "=" if i == self.posX and j == self.posY else " "
+        for row in range(9):
+            for column in range(9):
+                output += "||" if column % 3 == 0 else "|"
+                output += "=" if column == self.selectedColumn and row == self.selectedRow else " "
+                output += f"{self.puzzle[column][row]['value']}" if self.puzzle[column][row]['value'] != None else " "
+                output += f"({len(self.puzzle[column][row]['options'])})" if printOptions else ""
+                output += "=" if column == self.selectedColumn and row == self.selectedRow else " "
 
             output += "||\n"
 
-            if i % 3 == 2:
+            if row % 3 == 2:
                 output += "====================================================================\n" if printOptions else "=========================================\n"
             else:
                 output += "--------------------------------------------------------------------\n" if printOptions else "-----------------------------------------\n"
